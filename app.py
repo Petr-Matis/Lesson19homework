b31585c5b51fb4b553e698ef643e3f430cb41d3a
Petr Matis <dragon031088@gmail.com>
2023-02-25 15:15:44 +0300

    Lesson 19 homework

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from setup_db import db
 from views.directors import director_ns
 from views.genres import genre_ns
 from views.movies import movie_ns
+from views.auth import auth_ns
+from dao.model.user import User
+from implemented import user_service
+from views.users import user_ns
 
 
 def create_app(config_object):
@@ -21,10 +25,22 @@ def register_extensions(app):
     api.add_namespace(director_ns)
     api.add_namespace(genre_ns)
     api.add_namespace(movie_ns)
+    api.add_namespace(auth_ns)
+    api.add_namespace(user_ns)
 
 
 app = create_app(Config())
 app.debug = True
+
+with app.app_context():
+    db.create_all()
+
+    u1 = User(username="vasya", password=user_service.make_user_password_hash("my_little_pony"), role="admin")
+    u2 = User(username="oleg", password=user_service.make_user_password_hash("qwerty"), role="user")
+    u3 = User(username="oleg", password=user_service.make_user_password_hash("P@ssw0rd"), role="admin")
+
+    with db.session.begin():
+        db.session.add_all([u1, u2, u3])
 
 if __name__ == '__main__':
     app.run(host="localhost", port=10001, debug=True)
